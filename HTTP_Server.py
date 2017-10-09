6d55304c72854ad1bbb35226888a8b9bede770a4
Mauricio <masagi94@gmail.com>
2017-10-08 21:49:44 -0400

    copied in your code, changed so any port can be used

--- HTTP_Server.py
+++ HTTP_Server.py
@@ -1,34 +1,76 @@
+#Hamza Tanveer
+#ECE 465 Project 1
+
 #import socket module
 from socket import *
-import sys # In order to terminate the program
-serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-#Fill in start
-#Fill in end
-while True:
- #Establish the connection
- print('Ready to serve...')
- connectionSocket, addr = #Fill in start #Fill in end
- try:
- message = #Fill in start #Fill in end
- filename = message.split()[1]
- f = open(filename[1:])
- outputdata = #Fill in start #Fill in end
- #Send one HTTP header line into socket
- #Fill in start
- #Fill in end
- #Send the content of the requested file to the client
- for i in range(0, len(outputdata)):
- connectionSocket.send(outputdata[i].encode())
- connectionSocket.send("\r\n".encode())
+from threading import *
 
- connectionSocket.close()
- except IOError:
- #Send response message for file not found
- #Fill in start 
- #Fill in end
- #Close client socket
- #Fill in start
- #Fill in end
-serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data.
+#globals
+threadArray = []
+
+portNumber = 8000
+
+def main():
+	mainThread = Thread(target = startServer, name = 'ServerThread')
+	mainThread.setDaemon(True)
+	threadArray.append(mainThread)
+	threadArray[0].start()
+	print("Server thread initiated.")
+
+	serverQuit = False
+
+	while not serverQuit:
+		try:
+			pass
+		except KeyboardInterrupt:
+			print("Exiting...")
+			break
+
+
+def startServer():
+
+	#prepre socket
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket.bind((gethostname(), portNumber))
+	serverSocket.listen(1)
+
+	serverIp = gethostbyname(gethostname())
+	print("Server IP: " + serverIp)
+	print("Press CTRL + c to exit.\n")
+
+
+	while True:
+		#establish connection
+		connectionSocket, addr = serverSocket.accept()
+		threadName = "Thread - " + str(len(threadArray))
+		newThread = Thread(target = startConnectionThread, name = (threadName), args = [connectionSocket, addr])
+
+
+		threadArray.append(newThread)
+		newThread.start()
+
+	serverSocket.close()
+
+def startConnectionThread(connectionSocket, addr):
+	try:
+		message = connectionSocket.recv(4096)
+		filename = message.split()[1]
+		f = open(filename[1:])
+		outputdata = f.read()
+
+		#send 1 http header into socket
+		connectionSocket.send("HTTP/1.1 200 OK\r\nContent-type:text/html;charset=utf8\r\n\r\n")
+
+		#send content of req file to client
+		for i in range (0,  len(outputdata)):
+			connectionSocket.send(outputdata[i])
+		connectionSocket.close()
+
+	except IOError:
+		#send resp for file not found
+		connectionSocket.send("<!DOCTYPE html><html><body><h1>404 File Not Found</h1></body></html>")
+
+		#close client socket
+		connectionSocket.close()
+	
+main()
